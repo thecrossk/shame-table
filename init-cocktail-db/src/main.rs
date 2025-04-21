1cfb9717ff495acd867bc49f2403a712f9bfa4f3
lecrossk <crossk@firemail.de>
2025-04-21 23:46:40 +0200

    First docker file approach

--- init-cocktail-db/src/main.rs
+++ init-cocktail-db/src/main.rs
@@ -13,7 +13,8 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
 
     let database_url = env::var("DATABASE_URL").expect("Variable 'DATABASE_URL' is not set");
-    let database = db::init_database_connection(&database_url).await?;
+    let tmp = "postgres://crosskdb:ming@192.168.2.104:5432/cocktailbar";
+    let database = db::init_database_connection(&tmp).await?;
     tracing::info!("Connected to database on Pi, start running migrations...");
 
     sqlx::migrate!().run(&database).await.expect("Migrations failed");
